a3b9ea367b9e75c79accf700e7bb2f58c8606790
ShinsMohan <shinsmohan276@gmail.com>
2024-10-14 21:38:29 +0530

    fixed patch day 2

--- js/calender_editable.js
+++ js/calender_editable.js
@@ -1,22 +1,18 @@
 /** @odoo-module **/
 
-export class CalendarCommonPopover extends Component {
-    setup() {
-    }
+import { patch } from "@web/core/utils/patch";
+import { CalendarCommonPopover } from "@web/views/calendar/calendar_common/calendar_common_popover";
 
-    async onSave() {
-        const updatedData = {
-            title: this.record.title,
-            description: this.record.description,
-        };
-        await this.rpc({
-            model: 'calendar.event',
-            method: 'write',
-            args: [[this.record.id], updatedData],
-        });
-        this.closePopover();
-    }
-
-    closePopover() {
-    }
-}
+patch(CalendarCommonPopover.prototype, {
+    /**
+     * Customising meal calendar
+     *
+     * @private
+     * @override
+     */
+    onEditEvent() {
+        this.props.model.fieldsMode = 'edit';
+        this.props.editRecord(this.props.record);
+        this.props.close();
+    },
+});
