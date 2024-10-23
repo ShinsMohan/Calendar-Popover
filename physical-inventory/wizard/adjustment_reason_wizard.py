e74c7a97c7c7972bcbcc5fcf9f64c8ba95a010df
ShinsMohan <shinsmohan276@gmail.com>
2024-10-23 22:26:46 +0530

    [ADD]added wizard reason input

--- physical-inventory/wizard/adjustment_reason_wizard.py
+++ physical-inventory/wizard/adjustment_reason_wizard.py
@@ -1,14 +1,13 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class AdjustmentReasonWizard(models.TransientModel):
     _name = 'adjustment.reason.wizard'
     _description = 'Adjustment Reason Wizard'
 
-    reason = fields.Text(string='Reason', required=True)
+    reason = fields.Text(string='Adjustment Reason', required=True)
 
     def confirm_reason(self):
-        quant = self.env['stock.quant'].browse(self.env.context.get('active_id'))
-        move_lines = quant.inventory_quantity_set_ids.mapped('move_line_id')
+        inventory = self.env['stock.inventory'].browse(self.env.context.get('active_id'))
+        move_lines = inventory.move_ids_without_package.mapped('move_line_ids')
         for line in move_lines:
             line.adjustment_reason = self.reason
-
