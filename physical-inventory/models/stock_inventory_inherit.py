afef26d8bff2affb7665ca530d5022ad259c0520
ShinsMohan <shinsmohan276@gmail.com>
2024-10-24 22:12:00 +0530

    [ADD]added reason from wizard to field

--- physical-inventory/models/stock_inventory_inherit.py
+++ physical-inventory/models/stock_inventory_inherit.py
@@ -1,18 +1,17 @@
-from odoo import models, api
+from odoo import api, models
 
 class StockInventory(models.Model):
     _inherit = 'stock.inventory'
 
+    @api.model
     def _action_done(self):
-        # Call the original action_done method
-        res = super(StockInventory, self)._action_done()
-
-        # Return the wizard action after the original action completes
+        res = super()._action_done()
         return {
             'type': 'ir.actions.act_window',
             'name': 'Provide Adjustment Reason',
             'res_model': 'adjustment.reason.wizard',
             'view_mode': 'form',
             'target': 'new',
+            # 'view_id': self.id,
             'context': {'active_id': self.id},
         }
